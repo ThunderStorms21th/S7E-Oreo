f2502ea4d4357fa3c7bc4c2f836561a3240f8239
mawrick26 <mawrick26@gmail.com>
2018-05-27 10:02:04 +0200

    reduce spam & debug
    Signed-off-by: mawrick26 <mawrick26@gmail.com>

--- security/selinux_n/avc.c
+++ security/selinux_n/avc.c
@@ -626,8 +626,8 @@ static int avc_latest_notif_update(int seqno, int is_insert)
 	spin_lock_irqsave(&notif_lock, flag);
 	if (is_insert) {
 		if (seqno < avc_cache.latest_notif) {
-			printk(KERN_WARNING "SELinux: avc:  seqno %d < latest_notif %d\n",
-			       seqno, avc_cache.latest_notif);
+			pr_debug(KERN_WARNING "SELinux: avc:  seqno %d < latest_notif %d\n", 
+                     seqno, avc_cache.latest_notif);
 			ret = -EAGAIN;
 		}
 	} else {
@@ -744,6 +744,10 @@ noinline int slow_avc_audit(u32 ssid, u32 tsid, u16 tclass,
 {
 	struct common_audit_data stack_data;
 	struct selinux_audit_data sad;
+
+    /* Only log permissive=1 messages for SECURITY_SELINUX_DEVELOP */
+	if (!IS_ENABLED(CONFIG_SECURITY_SELINUX_DEVELOP) && denied && !result)
+		return 0;
 
 	if (!a) {
 		a = &stack_data;
